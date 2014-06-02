4a154a4363881ed4af9b5f5208c8abb0aba803c8
Dan Guest <dguest@cern.ch>
2014-06-02 19:41:20 -0400

    more cleanup

--- scripts/susy-fit-results.py
+++ scripts/susy-fit-results.py
@@ -5,98 +5,58 @@ to get the fit parameters as a yaml file.
 """
 
 def _dict_from_par(ip):
-  return {
-    'v': ip.getVal(),
-    'e': ip.getError(),
-    'errup': ip.getErrorLo(),
-    'errdn': ip.getErrorHi()
-    }
+    return {
+        'nom': ip.getVal(),
+        'unct': ip.getError(),
+        'unctdn': ip.getErrorLo(),
+        'unctup': ip.getErrorHi()
+      }
 
 def get_fit_results( filename, resultName="RooExpandedFitResult_afterFit"):
-  from scharmfit.utils import load_susyfit
-  load_susyfit()
+    from scharmfit.utils import load_susyfit
+    load_susyfit()
 
-  from ROOT import Util, gROOT
+    from ROOT import Util, gROOT
 
-  gROOT.Reset()
+    gROOT.Reset()
 
-  workspacename = 'w'
-  w = Util.GetWorkspaceFromFile(filename,workspacename)
+    workspacename = 'w'
+    w = Util.GetWorkspaceFromFile(filename,workspacename)
 
-  if w==None:
-    print "ERROR : Cannot open workspace : ", workspacename
-    sys.exit(1)
+    if w==None:
+        print "ERROR : Cannot open workspace : ", workspacename
+        sys.exit(1)
 
-  result = w.obj(resultName)
-  if result==None:
-    print "ERROR : Cannot open fit result ", resultName
-    sys.exit(1)
+    result = w.obj(resultName)
+    if result==None:
+        print "ERROR : Cannot open fit result ", resultName
+        sys.exit(1)
 
-  # calculate error per parameter on  fitresult
-  fpf = result.floatParsFinal()
-  fpi = result.floatParsInit()
+    # calculate error per parameter on  fitresult
+    fpf = result.floatParsFinal()
+    fpi = result.floatParsInit()
 
-  regSys = {}
+    regSys = {}
 
-  # set all floating parameters constant
-  for idx in range(fpf.getSize()):
-    parname = fpf[idx].GetName()
-    ip = fpi[idx]
-    ipv  = ip.getVal()
-    ipe  = ip.getError()
-    ipel = ip.getErrorLo()
-    ipeh = ip.getErrorHi()
-
-    fp = fpf[idx]
-    fpv  = fp.getVal()
-    fpe  = fp.getError()
-    fpel = fp.getErrorLo()
-    fpeh = fp.getErrorHi()
-
-    name = parname
-
-    regSys[name] = (ipv,ipe,ipel,ipeh,fpv,fpe,fpel,fpeh)
-
-  return regSys
-
-
-
-
-##################################
-
-# MAIN
+    # set all floating parameters constant
+    for idx in range(fpf.getSize()):
+        parname = fpf[idx].GetName()
+        regSys[parname] = {
+            'before': _dict_from_par(fpi[idx]),
+            'after': _dict_from_par(fpf[idx]),
+            }
+    return regSys
 
 if __name__ == "__main__":
-  import os, sys
-  import getopt
-  def usage():
-    print "Usage:"
-    print "PrintFitResult.py [-c channel] [-w workspace_afterFit] [-o outputFileName]\n"
-    print "Minimal set of inputs [-c channels] [-w workspace_afterFit]"
-    print "*** Options are: "
-    print "-c <analysis name>: single name accepted only (OBLIGATORY) "
-    print "-w <workspaceFileName>: single name accepted only (OBLIGATORY) ;   if multiple channels/regions given in -c, assumes the workspace file contains all channels/regions"
-    sys.exit(0)
+    import argparse, yaml
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('after_fit_workspace')
+    args = parser.parse_args()
+    showAfterFitError=True
+    resultName = 'RooExpandedFitResult_afterFit'
+    if not showAfterFitError:
+        resultName =  'RooExpandedFitResult_beforeFit'
 
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "o:c:w:m:f:s:%b")
-  except:
-    usage()
-  if len(opts)<1:
-    usage()
-
-  outputFileName="default"
-  method="1"
-  showAfterFitError=True
-  showPercent=False
-  for opt,arg in opts:
-    if opt == '-w':
-      wsFileName=arg
-
-  resultName = 'RooExpandedFitResult_afterFit'
-  if not showAfterFitError:
-    resultName =  'RooExpandedFitResult_beforeFit'
-
-  regSys = get_fit_results(wsFileName,resultName)
-  print regSys
+    regSys = get_fit_results(args.after_fit_workspace,resultName)
+    print yaml.dump(regSys, default_flow_style=False)
 

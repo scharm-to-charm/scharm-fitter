20ff6233348b782327cd1db4089fe77f010f87f8
Dan Guest <dguest@cern.ch>
2014-07-23 11:38:50 -0400

    add start of correlation matrix dumper to the fit-results script

--- scripts/susy-fit-results.py
+++ scripts/susy-fit-results.py
@@ -3,6 +3,7 @@
 Cleaned up version of PrintFitResults, I'm mainly interested in using it
 to get the fit parameters as a yaml file.
 """
+from __future__ import print_function
 
 def _dict_from_par(ip):
     return {
@@ -25,12 +26,12 @@ def get_fit_results( filename, resultName="RooExpandedFitResult_afterFit"):
     w = Util.GetWorkspaceFromFile(filename,workspacename)
 
     if w==None:
-        print "ERROR : Cannot open workspace : ", workspacename
+        print("ERROR : Cannot open workspace : ", workspacename)
         sys.exit(1)
 
     result = w.obj(resultName)
     if result==None:
-        print "ERROR : Cannot open fit result ", resultName
+        print("ERROR : Cannot open fit result ", resultName)
         sys.exit(1)
 
     # calculate error per parameter on  fitresult
@@ -48,6 +49,24 @@ def get_fit_results( filename, resultName="RooExpandedFitResult_afterFit"):
             }
     return regSys
 
+def get_corr_matrix( filename, resultName="RooExpandedFitResult_afterFit"):
+    # import itertools
+    from scharmfit.utils import load_susyfit
+    load_susyfit()
+    from ROOT import Util, gROOT
+
+    result = Util.GetWorkspaceFromFile(filename, 'w').obj(resultName)
+    paramenters = result.floatParsFinal()
+    n_par = paramenters.getSize()
+    for idx1 in range(n_par):
+        par1 = paramenters[idx1].GetName()
+        print(par1)
+        for idx2 in range(n_par):
+            par2 = paramenters[idx2].GetName()
+            print('{:.2f}'.format(result.correlation(par1, par2)), end=', ')
+        print('...')
+    print('')
+
 if __name__ == "__main__":
     import argparse, yaml
     parser = argparse.ArgumentParser(description=__doc__)
@@ -58,6 +77,7 @@ if __name__ == "__main__":
     if not showAfterFitError:
         resultName =  'RooExpandedFitResult_beforeFit'
 
+    # get_corr_matrix(args.after_fit_workspace, resultName)
     regSys = get_fit_results(args.after_fit_workspace,resultName)
-    print yaml.dump(regSys, default_flow_style=False)
+    print(yaml.dump(regSys, default_flow_style=False))
 
